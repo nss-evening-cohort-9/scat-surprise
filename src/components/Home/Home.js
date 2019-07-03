b01d856c12033fafdc0598fc7282fa01f7d1d12f
Zoe Ames <zoeames@gmail.com>
2019-07-02 20:34:06 -0500

    routing works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const orderId = '12345';
+    this.props.history.push(`/edit/${orderId}`);
+  }
+
   render() {
+    const singleLink = '/scat/12345';
     return (
       <div className="Home">
         <h1>Home</h1>
+        <button className="btn btn-danger" onClick={this.editEvent}>Edit a thing</button>
+        <Link to={singleLink}>View Single</Link>
       </div>
     );
   }
