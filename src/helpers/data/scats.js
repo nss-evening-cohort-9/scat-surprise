12d709455645f9c0ab795345832fc9980cdcaf7b
Zoe Ames <zoeames@gmail.com>
2019-07-08 19:08:56 -0500

    get request works

---
+++ src/helpers/data/scats.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
+
+const getMyScats = uid => new Promise((resolve, reject) => {
+  axios
+    .get(`${baseUrl}/scats.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((res) => {
+      const scats = [];
+      if (res.data !== null) {
+        Object.keys(res.data).forEach((fbKey) => {
+          res.data[fbKey].id = fbKey;
+          scats.push(res.data[fbKey]);
+        });
+      }
+      resolve(scats);
+    })
+    .catch(err => reject(err));
+});
+
+export default {
+  getMyScats,
+};
